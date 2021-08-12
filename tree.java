6537d9f8019e316b76c18b22a0d6c4135006b274
sarmand23 <sarmand23@outlook.com>
2021-08-12 13:51:57 -0400

    Create tree.java
    Planting a tree in Java.

---
+++ tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args){
+    // Introduce myself and print a tree to the screen
+    System.out.println("Hello, this is Spencer and this is a Java example!");
+    System.out.println("Let us plant a tree.");
+    System.out.println("   *  ");
+    System.out.println("   ** ");
+    System.out.println("   *** ");
+    System.out.println("  *****  ");
+    System.out.println(" *******  ");
+    System.out.println("    ||  ");    
+  }
+}
